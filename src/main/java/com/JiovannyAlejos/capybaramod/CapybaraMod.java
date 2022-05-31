b6adc54ee5136a1d8961b11e2797ad6c17661793
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2022-05-30 19:48:23 -0700

    capybaras now in the game and yes

--- src/main/java/com/JiovannyAlejos/capybaramod/CapybaraMod.java
+++ src/main/java/com/JiovannyAlejos/capybaramod/CapybaraMod.java
@@ -1,18 +1,13 @@
 package com.JiovannyAlejos.capybaramod;
 
+import com.JiovannyAlejos.capybaramod.entity.EntityInit;
+import com.JiovannyAlejos.capybaramod.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -28,15 +23,9 @@ public class CapybaraMod {
     public CapybaraMod() {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        eventBus.addListener(this::setup);
-
+        ModItems.register(eventBus);
+        EntityInit.ENTITIES.register(eventBus);
         // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        //MinecraftForge.EVENT_BUS.register(this);
     }
 }
---
+++ src/main/java/com/JiovannyAlejos/capybaramod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.JiovannyAlejos.capybaramod.item;
+
+import com.JiovannyAlejos.capybaramod.CapybaraMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> items =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CapybaraMod.MOD_ID);
+
+    public static final RegistryObject<Item> ITEM_REGISTRY = items.register("water_spout",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        items.register(eventBus);
+        items.register("test", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    }
+}
